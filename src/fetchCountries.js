435b778073495407d0c4686b1826306f6d3ba30c
Klymuk <114335106+Klymykk@users.noreply.github.com>
2023-05-07 19:33:10 +0300

    commit

---
+++ src/fetchCountries.js
@@ -0,0 +1,15 @@
+const BASE_URL = "https://restcountries.com/v3.1/";
+const END_POINT = "name/";
+
+function fetchCountries(state) {
+    const URL = `${BASE_URL}${END_POINT}${state}?fields=name,capital,population,flags,languages`;
+    return fetch(URL).then(resp => {
+        if (!resp.ok) {
+            throw new Error(resp.statusText);
+        }
+
+        return resp.json()
+    });
+};
+
+export { fetchCountries };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import { fetchCountries } from "./fetchCountries";
 
+const input = document.querySelector("#search-box")
 const DEBOUNCE_DELAY = 300;
+
+input.addEventListener( "input", (event) =>{
+    console.log(event.target.value);
+    fetchCountries(event.target.value).then(data => console.log(data)).catch(err => console.log(err))
+});
+
+// function getState(event) {
+//     console.log(event);
+//     fetchCountries(event.value).then(data => console.log(data)).catch(err => console.log(err))
+// }
+
